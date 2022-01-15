6076d7aa28d3e8c3c4346eeeeaf4e6ac32d462a7
neal christopher jamieson <neal.c.jamieson@outlook.com>
2022-01-15 04:26:35 -0600

    Discord OAuth2 now successfully working. Begun work on authorization policies

--- TowerOfDaedelus_WebApp/Pages/Index.cshtml.cs
+++ TowerOfDaedelus_WebApp/Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TowerOfDaedelus_WebApp.Pages
 {
+    [AllowAnonymous]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
--- TowerOfDaedelus_WebApp/Pages/Privacy.cshtml.cs
+++ TowerOfDaedelus_WebApp/Pages/Privacy.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TowerOfDaedelus_WebApp.Pages
 {
+    [AllowAnonymous]
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
